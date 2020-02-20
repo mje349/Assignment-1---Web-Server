a7a940518cfbfb4f315c4db4e507edd9a9dbdeb7
Montana E <montana@pdhq.dirty.or>
2020-02-20 14:11:48 -0800

    initialized TCPClient.py

---
+++ TCPClient.py
@@ -0,0 +1,22 @@
+#Author: Montana Esguerra
+#Date: 2/20/20
+#Filename: TCPClient.py
+#Description: Sample TCP Client program from the textbook
+
+from socket import *
+
+from pip._vendor.distlib.compat import raw_input
+
+SERVER_NAME = 'servername'
+SERVER_PORT = 12000
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((SERVER_NAME, SERVER_PORT))
+
+sentence = raw_input('Input lowercase sentence:')
+client_socket.send(sentence.encode())
+
+modified_sentence = client_socket.recv(1024)
+print('From Server: ', modified_sentence.decode())
+
+client_socket.close()
