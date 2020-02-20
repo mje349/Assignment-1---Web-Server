362094f724b2f0c0385150c6f5f558a200c0abfe
Montana E <montana@pdhq.dirty.or>
2020-02-20 09:52:42 -0800

    sample code copied

--- UDPClient.py
+++ UDPClient.py
@@ -4,3 +4,16 @@
 #Description: Sample UDP Client program from the textbook
 
 from socket import *
+
+SERVER_NAME = 'hostname'
+PORT = 12000
+
+client_socket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Input lowercase sentence:')
+client_socket.sendto(message.encode(), (SERVER_NAME, PORT))
+modifiedMessage, serverAddress = client_socket.recvfrom(2048)
+
+print(modifiedMessage.decode())
+
+client_socket.close()
+
