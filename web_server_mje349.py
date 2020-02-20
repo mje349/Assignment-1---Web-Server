37e3f6448e5e66f378793071f5a78201e4d617ce
mesgu2@pdx.edu <mesgu002@DESKTOP-8VG7B23.localdomain>
2020-02-19 21:33:41 -0800

    server is able to listen on port 8080

--- web_server_mje349.py
+++ web_server_mje349.py
@@ -13,9 +13,10 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
 
-#I arbitrarily choose port 8080 for testing
-serverPort = 8080
-serverSocket.bind(('localhost', serverPort))
+HOST = 'localhost'
+PORT = 8080
+
+serverSocket.bind((HOST, PORT))
 
 #The parameter to listen specifies the maximum number of queued connections
 serverSocket.listen(1)
@@ -24,30 +25,41 @@ serverSocket.listen(1)
 
 while True:
     #Establish the connection
-    print('Ready to serve...')
+    print('Ready to serve on port ' + str(PORT))
 
     #"Client Socket"
-    connectionSocket, addr = '''Fill in start ''' serverSocket.accept() ''''Fill in end'''
+    connectionSocket, addr = serverSocket.accept()
+
+    #TEST See who connected
+    print('Connection made: ' + str(addr))
     try:
-        message = '''Fill in start''' connectionSocket.recv(1024).decode '''Fill in end'''
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = '''Fill in start ''' "HelloWorld.html"  '''#Fill in end'''
+        message = connectionSocket.recv(1024).decode
+        #TEST see what was received
+        print('Server received', repr(message))
+
+        #TEST explicitly use HelloWorld.html
+        filename = 'HelloWorld.html'
+        f = open(filename, 'rb')
+        #filename = message.split(' ')
+        #f = open(filename[1:])
+        outputdata = f.read(1024)
 
         #Send one HTTP header line into socket
         #Fill in start
-        connectionSocket.send('HTTP/1.1 200 OK\r\n')
+        connectionSocket.send(b'HTTP/1.1 200 OK\r\n')
         #Fill in end
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
+        f.close()
 
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
     except IOError:
         #Send response message for file not found (404)
         #Fill in start
+        connectionSocket.send('HTTP/1.1 404 NOT FOUND\r\n')
 
         #Fill in end
 
