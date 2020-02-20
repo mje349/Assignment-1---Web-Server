f54c83535870f868645602eb2ffff6576920c6b7
Montana E <montana@pdhq.dirty.or>
2020-02-20 14:32:12 -0800

    changed 1024 to 2048

--- TCPClient.py
+++ TCPClient.py
@@ -16,7 +16,7 @@ client_socket.connect((SERVER_NAME, SERVER_PORT))
 sentence = raw_input('Input lowercase sentence:')
 client_socket.send(sentence.encode())
 
-modified_sentence = client_socket.recv(1024)
+modified_sentence = client_socket.recv(2048)
 print('From Server: ', modified_sentence.decode())
 
 client_socket.close()
