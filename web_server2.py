26c598c90d1a6431212f9a988e9b85fa09420e50
Montana E <montana@pdhq.dirty.or>
2020-02-20 16:34:37 -0800

    updated web_server2 to stable

--- web_server2.py
+++ web_server2.py
@@ -7,7 +7,7 @@
 from socket import *
 import sys # In order to terminate the program
 
-SERVER_PORT = 12000
+SERVER_PORT = 12001
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #Prepare a sever socket
@@ -24,11 +24,12 @@ while True:
         message = connectionSocket.recv(2048).decode() #Fill in start    #Fill in end
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
+        outputdata = f.read() #Fill in start     #Fill in end
 
         #Send one HTTP header line into socket
         #Fill in start
-
+        httpOK = 'HTTP/1.1 200 OK\r\n'
+        connectionSocket.send(httpOK.encode())
         #Fill in end
 
         #Send the content of the requested file to the client
@@ -40,12 +41,14 @@ while True:
     except IOError:
         #Send response message for file not found (404)
         #Fill in start
+        httpNotFound = 'HTTP/1.1 404 NOT FOUND\r\n'
+        connectionSocket.send(httpNotFound.encode())
 
         #Fill in end
 
         #Close client socket
         #Fill in start
-
+    connectionSocket.close()
         #Fill in end
 serverSocket.close()
 sys.exit()  #Terminate the program after sending the corresponding data
