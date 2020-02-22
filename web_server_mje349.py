d58e26fdee4af6c179b0f5ad9dda78e70292944d
mesgu2@pdx.edu <mesgu002@DESKTOP-8VG7B23.localdomain>
2020-02-21 23:31:25 -0800

    removed old version of the web server and renamed the new version to web_server_mje349.py

--- web_server_mje349.py
+++ web_server_mje349.py
@@ -1,72 +1,77 @@
-'''
-Author: Montana Esguerra
-Email: mje349@nyu.edu
-'''
-
+#Author: Montana Esguerra
+#Date: 2/20/20
+#Filename: web_server2.py
+#Description:
+#This is a simple web server that allows for only 1 client to connect at a time.
+#This server runs off of localhost on port 12001
+#Currently, the server only serves two html pages: HelloWorld.html and notFound.html
+#The notFound.html page is served anytime a page not in the directory is requested - this includes index.html
+#This is a TCP Web Server - it works with Firefox, Chrome, and Edge without issue.
+#To get the notFound.html to display on Internet Explorer, go into settings - Internet Options - Advanced tab -
+#Scroll down and uncheck Show friendly HTTP Error Messages
 
 #import socket module
 from socket import *
 import sys # In order to terminate the program
 
+SERVER_PORT = 12001
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #Prepare a sever socket
 #Fill in start
-
-HOST = 'localhost'
-PORT = 8080
-
-serverSocket.bind((HOST, PORT))
-
-#The parameter to listen specifies the maximum number of queued connections
-serverSocket.listen(1)
-
+serverSocket.bind(('', SERVER_PORT))
+serverSocket.listen(1) #So far only allows for 1 connection at a time
 #Fill in end
 
 while True:
     #Establish the connection
-    print('Ready to serve on port ' + str(PORT))
-
-    #"Client Socket"
-    connectionSocket, addr = serverSocket.accept()
-
-    #TEST See who connected
-    print('Connection made: ' + str(addr))
+    print('Ready to serve on port ' + str(SERVER_PORT))
+    connectionSocket, addr = serverSocket.accept() #Fill in start      #Fill in end
     try:
-        message = connectionSocket.recv(1024).decode
-        #TEST see what was received
-        print('Server received', repr(message))
-
-        #TEST explicitly use HelloWorld.html
-        filename = 'HelloWorld.html'
-        f = open(filename, 'rb')
-        #filename = message.split(' ')
-        #f = open(filename[1:])
-        outputdata = f.read(1024)
+        message = connectionSocket.recv(2048).decode() #Fill in start    #Fill in end
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read() #Fill in start     #Fill in end
+        f.close()
 
         #Send one HTTP header line into socket
         #Fill in start
-        connectionSocket.send(b'HTTP/1.1 200 OK\r\n')
+        httpOK = 'HTTP/1.1 200 OK\r\n'
+        connectionSocket.send(httpOK.encode())
+        connectionSocket.send('Content-Type: text/html\n'.encode())
+        connectionSocket.send('\n'.encode())
         #Fill in end
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
-        f.close()
 
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
     except IOError:
         #Send response message for file not found (404)
         #Fill in start
-        connectionSocket.send('HTTP/1.1 404 NOT FOUND\r\n')
 
+        #Prepare to send the 404 Not Found html page
+        not_found = open('notFound.html')
+        outputNotFound = not_found.read()
+        not_found.close()
+
+
+        httpNotFound = 'HTTP/1.1 404 NOT FOUND\r\n'
+        connectionSocket.send(httpNotFound.encode())
+        connectionSocket.send('Content-Type: text/html\n'.encode())
+        connectionSocket.send('\n'.encode())
+
+        for i in range(0, len(outputNotFound)):
+            connectionSocket.send(outputNotFound[i].encode())
+
+        connectionSocket.send("\r\n".encode())
         #Fill in end
 
         #Close client socket
         #Fill in start
-        connectionSocket.close()
-
+    connectionSocket.close()
         #Fill in end
 serverSocket.close()
 sys.exit()  #Terminate the program after sending the corresponding data
